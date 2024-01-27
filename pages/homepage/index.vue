65a240930352bf9af65c04362f5d8b0dc0890157
nutthakanT <98002147+nutthakanT@users.noreply.github.com>
2024-01-27 16:16:22 +0000

    day1

---
+++ pages/homepage/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-col>
+        
+        <v-card 
+        icon="login"
+        title="Location" subtitle="Coffee bean locations in the North" class="mx-auto" max-width="1440">
+            <v-card-actions>
+                <v-col cols="4" sm="6" md="4">
+                    <v-btn variant="tonal" block rounded="xl">View All</v-btn>
+                </v-col>
+            </v-card-actions>
+
+        </v-card>
+</v-col>
+    </div>
+</template>
