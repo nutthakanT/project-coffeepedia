bf14df8b9fd8e25693c7a1da080ee300db54f155
nutthakanT <98002147+nutthakanT@users.noreply.github.com>
2024-01-28 08:31:13 +0000

    15:31

--- pages/homepage/index.vue
+++ pages/homepage/index.vue
@@ -1,23 +1,27 @@
 <template>
+    <div class="d-flex mb-4">
+        
+            <v-img height="50" src="@/assets/blackloco1.png" class="ma-2 " />
+            <v-img height="30" src="@/assets/blackloco2.png" class="ma-2 mr-2" />
+        
+    </div>
     <div>
+        <v-divider color="brown"> </v-divider>
         <v-card>
-    <v-tabs
-      v-model="tab"
-      color="rgb(240, 200, 165)"
-      align-tabs="center"
-    >
-      <v-tab :value="1">Home</v-tab>
-      <v-tab :value="2">Type coffee beans</v-tab>
-      <v-tab :value="3">Blog</v-tab>
-      <v-tab :value="4">News</v-tab>
-      <v-tab :value="5">Contact</v-tab>
-    </v-tabs>
-  </v-card>
+            <v-tabs v-model="tab" color="rgb(240, 200, 165)" align-tabs="center">
+                <v-tab :value="1">Home</v-tab>
+                <v-tab :value="2">Type coffee beans</v-tab>
+                <v-tab :value="3">Blog</v-tab>
+                <v-tab :value="4">News</v-tab>
+                <v-tab :value="5">Contact</v-tab>
+            </v-tabs>
+        </v-card>
     </div>
     <div>
         <v-col>
-            <v-img height="200" src="@/assets/location.gif" />
-            <v-card title="Location" subtitle="Coffee bean locations in the North" class="mx-auto" max-width="1440">
+            <v-card title="Location" subtitle="Coffee bean locations in the North" class="mx-auto" max-width="1440"
+                color="rgba(240, 200, 80, 0.2)">
+                <v-img height="200" src="@/assets/location.gif" />
                 <v-card-actions>
                     <v-col cols="4" sm="6" md="4">
                         <v-btn variant="tonal" block rounded="xl">View All</v-btn>
@@ -30,9 +34,9 @@
 </template>
 
 <script>
-  export default {
+export default {
     data: () => ({
-      tab: null,
+        tab: null,
     }),
-  }
+}
 </script>
